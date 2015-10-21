77e288c3b3102f11eef38ea213970e7931fb3dc8
Amanda Astey <aastey@aastey-06017.gateway.sonic.net>
2015-10-20 20:20:57 -0700

    filling in the faqs and doing some small style updates

--- js/main.js
+++ js/main.js
@@ -62,5 +62,22 @@ function scrollNav() {
 }
 scrollNav();
 
+function scrollLink() {
+  $('.target-link').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 0
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollLink();
+
 
 });
